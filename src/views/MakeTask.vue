b0723b10f3ca89743eecbff1a487239227278aab
Vetta1337 <elkido@hotmail.no>
2024-02-15 22:16:21 +0100

    Added several functions

---
+++ src/views/MakeTask.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- Your template code goes here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MakeTask',
+  data() {
+    return {
+      // Your data properties go here
+    };
+  },
+  methods: {
+    // Your methods go here
+  },
+  mounted() {
+    // Code to run when the component is mounted
+  },
+};
+</script>
+
+<style scoped>
+/* Your component-specific styles go here */
+</style>
